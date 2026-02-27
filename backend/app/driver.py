4451ce84522c84e59887075ed7aa763cb8479c35
TsaariII <nikolai.zharkevich@gmail.com>
2026-02-27 10:38:55 +0200

    fix: movement logic fixed to work on basic level

--- backend/app/driver.py
+++ backend/app/driver.py
@@ -19,56 +19,148 @@ class Driver:
         self.starting_tile: int = 0
         self.tires: Tire = Tire(tires, weather)
     
-    # def check_movement(self, track: Track) -> None:
-    #     """Validate movement to the next square"""
-    #     if 
-
-    def move_forward(self, track: Track) -> None:
+    def move_forward(self, track: Track, crash: int, gear: str) -> None:
         """Move the driver one square forward along the current lane."""
         tile = track.tile(self.tile_idx)
+        current = tile.square_data(self.lane_idx, self.square_idx)
+        print(f"*BEGIN* Position on track:\n\tTile: {track.tile(self.tile_idx).number}\n\tLane: {self.lane_idx}\n\tSquare: {self.square_idx}")
         lane_squares = tile.lane_squares.get(self.lane_idx)
         if not lane_squares:
             print("Error: driver is in a non-existing or empty lane!")
             return
-        # self.check_movement(track)
-        if self.square_idx + 1 < len(lane_squares):
+        def as_list(v):
+            if v is None:
+                return []
+            return v if isinstance(v, list) else [v]
+        warning_exit_lanes = as_list(current.get("exit_warning_lane"))
+        max_gear_exit = current.get("max_gear_exit")
+        max_gear_exit_lanes = as_list(current.get("max_gear_exit_lane"))
+        if current.get("exit_warning") and self.lane_idx in warning_exit_lanes:
+            crash += 1
+        if current.get("max_gear_exit_lanes") and self.lane_idx in max_gear_exit_lanes:
+            if max_gear_exit is not None and int(self.current_gear) > max_gear_exit:
+                crash += 3
+                if self.square_idx + 1 >= len(tile.lane_squares):
+                    self.tile_idx += 1
+                    self.square_idx = 0
+                    print(
+                            f"{self.name} moved to tile {self.tile_idx + 1}"
+                            f"({track.tile(self.tile_idx + 1).color}) in lane {self.lane_idx}"
+                        )
+                else:
+                    self.square_idx += 1
+                    print(f"{self.name} moved forward one square")
+                return
+        # print(f"Square: {self.square_idx}")
+        # print(f"Lane len: {len(lane_squares)}")
+        if self.square_idx + 1 >= len(lane_squares):
+            if self.tile_idx == 22:
+                tile = track.tile(1)
+                self.square_idx = 0
+                next = tile.square_data(self.lane_idx, 1)
+                print(
+                    f"{self.name} moved to tile {self.tile_idx + 1}"
+                    f"({track.tile(self.tile_idx + 1).color}) in lane {self.lane_idx}"
+                )
+                if next.get("inside") is None and next.get("merge"):
+                    print("!!!! HERE 1 !!!!")
+                    merge_id = next.get("merge")
+                    if merge_id == "M1":
+                        self.lane_idx = 1
+                        next = tile.square_data(self.lane_idx, self.square_idx)
+                        # print(f"Tile index: {self.tile_idx}")
+                        # print(f"Lane index: {self.lane_idx}")
+                        # print(f"Square index: {self.square_idx}")
+                    if merge_id == "M3":
+                        self.lane_idx = 3
+                        next = tile.square_data(self.lane_idx, self.square_idx)
+            else:
+                tile = track.tile(self.tile_idx + 1)
+                self.square_idx = 0
+                self.tile_idx += 1
+                # print(f"Tile index: {self.tile_idx}")
+                # print(f"Lane index: {self.lane_idx}")
+                # print(f"Square index: {self.square_idx}")
+                next = tile.square_data(self.lane_idx, self.square_idx)
+                print(
+                    f"{self.name} moved to tile {self.tile_idx + 1}"
+                    f"({track.tile(self.tile_idx + 1).color}) in lane {self.lane_idx}"
+                )
+                if next.get("inside") is None and next.get("merge"):
+                    print("!!!! HERE 2 !!!!")
+                    merge_id = next.get("merge")
+                    if merge_id == "M1":
+                        self.lane_idx = 1
+                        next = tile.square_data(self.lane_idx, self.square_idx)
+                        # print(f"Tile index: {self.tile_idx}")
+                        # print(f"Lane index: {self.lane_idx}")
+                        # print(f"Square index: {self.square_idx}")
+                    if merge_id == "M3":
+                        self.lane_idx = 3
+                        next = tile.square_data(self.lane_idx, self.square_idx)
+        else:
             self.square_idx += 1
+            print(f"{self.name} moved forward one square")
+            next = tile.square_data(self.lane_idx, self.square_idx)
+            # print(f"Next tile: {next}")
+            if next.get("inside") is None and next.get("merge"):
+                print("!!!! HERE 3 !!!!")
+                merge_id = next.get("merge")
+                if merge_id == "M1":
+                    self.lane_idx = 1
+                    next = tile.square_data(self.lane_idx, self.square_idx)
+                    # print(f"Tile index: {self.tile_idx}")
+                    # print(f"Lane index: {self.lane_idx}")
+                    # print(f"Square index: {self.square_idx}")
+                if merge_id == "M3":
+                    self.lane_idx = 3
+                    next = tile.square_data(self.lane_idx, self.square_idx)
+        warning_enter_lanes = as_list(current.get("enter_warning_lane"))
+        max_gear_enter = current.get("max_gear_enter")
+        max_gear_enter_lanes = as_list(current.get("max_gear_enter_lane"))
+        if next.get("enter_warning") and self.lane_idx in warning_enter_lanes:
+            crash += 1
+        if next.get("max_gear_enter_lanes") and self.lane_idx in max_gear_enter_lanes:
+            if max_gear_enter is not None and int(self.current_gear) > max_gear_enter:
+                crash += 3
+                return
+        max_gear = next.get("max_gear")
+        if max_gear is not None and max_gear < int(gear):
+            crash += 3
             return
-        self.tile_idx += 1
-        if self.tile_idx >= track.length:
-            self.lap += 1
-            self.tile_idx = 0
-        next_tile = track.tile(self.tile_idx)
-        next_lane_squares = next_tile.lane_squares.get(self.lane_idx)
-        if not next_lane_squares:
-            print(f"Error: lane {self.lane_idx} does not exist in tile {self.tile_idx}!")
-            return
-        self.square_idx = 0
-        print(
-            f"{self.name} moved to tile {self.tile_idx + 1}"
-            f"({next_tile.color}) in lane {self.lane_idx}"
-        )
+        max_gear_else_warning = next.get("max_gear_else_warning")
+        if max_gear_else_warning is not None and int(gear) > max_gear_else_warning:
+            crash += 1
+        print(f"*END* Position on track:\n\tTile: {track.tile(self.tile_idx).number}\n\tLane: {self.lane_idx}\n\tSquare: {self.square_idx}")
 
-    def change_lane(self, track: Track) -> None:
+
+    def change_lane(self, track: Track, crash: int) -> None:
         """Change lane left or right on the current tile."""
         tile = track.tile(self.tile_idx)
         while True:
             try:
-                direction = int(
-                    input('Enter lane change direction (1: left, -1: right): ')
+                lane = int(
+                    input('Enter lane change number (1, 2, 3): ')
                 )
             except ValueError:
                 print('Invalid lane change input! Try again.')
                 continue
-            new_lane = self.lane_idx + direction
-            print(f"new lane: {new_lane}")
-            if 1 <= new_lane <= tile.lanes:
-                self.lane_idx = new_lane
+            prev_len = tile.lane_squares.get(self.lane_idx, [])
+            new_len =  tile.lane_squares.get(lane, [])
+            # print(f"new lane: {lane}")
+            if 1 <= lane <= tile.lanes:
+                if len(new_len) > len(prev_len):
+                    print(f"Lane lenths\n\tPrev: {len(prev_len)}\n\tNew: {len(new_len)}")
+                    self.square_idx += 1
+                if len(new_len) < len(prev_len):
+                    print(f"Lane lenths\n\tPrev: {len(prev_len)}\n\tNew: {len(new_len)}")
+                    self.square_idx -= 1
+                self.lane_idx = lane
                 print(f"{self.name} switched to lane {self.lane_idx}")
                 break
             print("Invalid lane! Try again.")
 
-def change_lane_or_move(track: Track, driver: Driver) -> None:
+def change_lane_or_move(track: Track, driver: Driver, crash: int, gear: str) -> None:
     """Ask the player whether to move forward or change lane for this driver."""
     while True:
         try:
@@ -82,6 +174,7 @@ def change_lane_or_move(track: Track, driver: Driver) -> None:
             break
         print('Invalid action! Try again')
     if choice == 1:
-        driver.move_forward(track)
+        driver.move_forward(track, crash, gear)
     else:
-        driver.change_lane(track)
+        driver.change_lane(track, crash)
+        driver.move_forward(track, crash, gear)
